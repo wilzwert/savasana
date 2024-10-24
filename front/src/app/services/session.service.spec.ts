99d455fc537f28c4c7566f433fb712b5ab231e62
Wilhelm Zwertvaegher <wilzwert@gmail.com>
2024-10-24 14:51:53 +0200

    test: add tests for SessionService

--- front/src/app/services/session.service.spec.ts
+++ front/src/app/services/session.service.spec.ts
@@ -2,16 +2,60 @@ import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { SessionService } from './session.service';
+import { Observable } from 'rxjs';
+import { SessionInformation } from '../interfaces/sessionInformation.interface';
 
 describe('SessionService', () => {
   let service: SessionService;
+  let mockUser:SessionInformation;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(SessionService);
+    mockUser = {
+        token: 'abcd1234',
+        type: 'Bearer',
+        id: 1,
+        username: 'test',
+        firstName: 'Test',
+        lastName: 'User',
+        admin: false
+    };
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return logged status as an observable', () => {
+    expect(service.$isLogged()).toBeTruthy();
+    expect(service.$isLogged()).toBeInstanceOf(Observable);
+  });
+
+  it('initial logged status should be false', () => {
+    expect(service.isLogged).toBe(false);
+    service.$isLogged().subscribe((value) => {
+      expect(value).toBe(false);
+    });
+  })
+
+  it('should return logged status true after login', () => {
+    service.logIn(mockUser);
+    expect(service.isLogged).toBe(true);
+    expect(service.sessionInformation).toBe(mockUser);
+    service.$isLogged().subscribe((value) => {
+      expect(value).toBe(true);
+    });
+  })
+
+  it('should return logged status false after logout', () => {
+    service.logIn(mockUser);
+    service.logOut();
+    expect(service.sessionInformation).toBeUndefined();
+    expect(service.isLogged).toBe(false);
+
+    service.$isLogged().subscribe((value) => {
+        expect(value).toBe(false);
+    });
   });
 });
